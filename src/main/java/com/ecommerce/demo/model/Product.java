04441672739701b8265c589b8d412a2b74dc4671
suresmo <monisha.m.suresh@mercedes-benz.com>
2024-03-18 00:41:45 +0530

    First commit

---
+++ src/main/java/com/ecommerce/demo/model/Product.java
@@ -0,0 +1,63 @@
+package com.ecommerce.demo.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "products")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private @NotNull String name;
+
+    private @NotNull double price;
+    private @NotNull String description;
+
+    // Many to one relationship
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    Category category;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+}
